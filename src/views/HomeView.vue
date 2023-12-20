dfb79cbfe815d685c4787076cc88b3edce3f3804
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2023-12-20 23:03:22 +0100

    add coming soon page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import '../components/Home.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Home />
   </main>
 </template>
